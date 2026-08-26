39bc96d1410f537cdd1f453e3bb8dd9dee6b802a
SPECTRUM00-prog <diegoesca2310@gmail.com>
2026-08-26 02:21:41 -0600

    feat: Renderizar landing page oficial de Stremio en /

--- api/index.js
+++ api/index.js
@@ -1,10 +1,17 @@
 ﻿const express = require("express");
-const { getRouter } = require("stremio-addon-sdk");
+const { getRouter, landingTemplate } = require("stremio-addon-sdk");
 const addonInterface = require("../addon.js");
 
 const app = express();
 const router = getRouter(addonInterface);
 
-app.use("/", router);
+// Renderizar la página oficial de bienvenida con el botón de Instalar en /
+app.get("/", (req, res) => {
+    const landingHTML = landingTemplate(addonInterface.manifest);
+    res.setHeader("content-type", "text/html");
+    res.end(landingHTML);
+});
+
+app.use(router);
 
 module.exports = app;
